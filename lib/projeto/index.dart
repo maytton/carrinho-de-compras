0d4181e13bbe9ee906f1039ca588977a1a926408
Maytton <maytton0000@gmail.com>
2024-03-11 11:12:36 -0300

    Adicionado uma AppBar com título da página.

--- lib/projeto/index.dart
+++ lib/projeto/index.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CarinhoCompras extends StatelessWidget {
   const CarinhoCompras({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 20,
+        title: const Text('Carrinho de Compras'),
+      ),
+    );
   }
 }
