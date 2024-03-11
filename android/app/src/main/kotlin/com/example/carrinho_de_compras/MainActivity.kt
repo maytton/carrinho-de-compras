1d1fb46245e9d3162df459a5e07c71f9be65dc3a
Maytton <maytton0000@gmail.com>
2024-03-11 10:55:00 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/example/carrinho_de_compras/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.carrinho_de_compras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/projeto/index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CarinhoCompras extends StatelessWidget {
+  const CarinhoCompras({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
