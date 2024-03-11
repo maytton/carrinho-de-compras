76e7235a4a7d1f6dc58301bfdbe0771ab72dbf52
Maytton <maytton0000@gmail.com>
2024-03-11 11:31:01 -0300

    Nessa alteração eu adicionei um Drawer no canto superior esquerdo, aonde vai ser exibido a informações da conta do usuário logado.

--- lib/projeto/index.dart
+++ lib/projeto/index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,9 +9,23 @@ class CarinhoCompras extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 125, 133, 243),
         elevation: 20,
         title: const Text('Carrinho de Compras'),
+      ),
+      drawer: Drawer(
+        elevation: 50,
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text('Maytton Nascimento'),
+              accountEmail: Text('maytton7@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: Text('MN'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
